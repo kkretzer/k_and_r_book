f1cd8a6a99df31baf68de636af4cab01b1ea00f1
Kurt Kretzer <kurt.kretzer@gmail.com>
2012-06-13 21:05:51 -0500

    multidimensional arrays, ex5-8

---
+++ chapter5/5.7.multidimensional_arrays.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int day_of_year(int year, int month, int day);
+void month_day(int year, int yearday, int *pmonth, int *pday);
+
+main()
+{
+    int *m, *d, mm=0, dd=0;
+    m = &mm;
+    d = &dd;
+
+    printf("day of year for %d/%0d/%0d : %d\n", 2012, 1, 1, day_of_year(2012, 1, 1));
+    printf("day of year for %d/%0d/%0d : %d\n", 2012, 2, 29, day_of_year(2012, 2, 29));
+    printf("day of year for %d/%0d/%0d : %d\n", 2012, 6, 12, day_of_year(2012, 6, 12));
+    printf("day of year for %d/%0d/%0d : %d\n", 2012, 12, 31, day_of_year(2012, 12, 31));
+    fflush(stdout);
+
+    month_day(2011, 1, m, d);
+    printf("month day for day %d of %d : %d/%d\n", 1, 2011, *m, *d);
+    month_day(2011, 31, m, d);                  
+    printf("month day for day %d of %d : %d/%d\n", 31, 2011, *m, *d);
+    month_day(2011, 59, m, d);                  
+    printf("month day for day %d of %d : %d/%d\n", 59, 2011, *m, *d);
+    month_day(2011, 365, m, d);                 
+    printf("month day for day %d of %d : %d/%d\n", 365, 2011, *m, *d);
+}
+
+
+static char daytab[2][13] = {
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+/* day_of_year: set day of year from month & day */
+int day_of_year(int year, int month, int day)
+{
+    int i, leap;
+
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    for (i = 1; i < month; i++)
+    {
+        day += daytab[leap][i];
+    }
+    return day;
+}
+
+/* month_day: set month, day from day of year */
+void month_day(int year, int yearday, int *pmonth, int *pday)
+{
+    int i, leap;
+
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    for (i = 1; yearday > daytab[leap][i]; i++)
+    {
+        yearday -= daytab[leap][i];
+    }
+    *pmonth = i;
+    *pday = yearday;
+}
